adbdf1f7ba3649068b05d888689ebe0fd735ed4b
PRANZALSHARMA <96528630+PRANZALSHARMA@users.noreply.github.com>
2022-01-10 18:03:13 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,9 +0,0 @@
-#Q4. Write a python program to enter marks of 5 students into a list and display them in sorted manner.
-
-lst=[]
-
-for i in range(5):
-    marks=float(input("Student marks: "))
-    lst.append(marks)
-lst.sort()
-print("\nSorted List Of marks:-",lst,sep='\n')
